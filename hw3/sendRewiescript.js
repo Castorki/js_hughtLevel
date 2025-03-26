6ef21de7f199d3f14d20aa6aae41734b66f23848
Darya Kauzova <amayasuzuki12@gmail.com>
2025-03-26 12:33:03 +0300

    final test

--- hw3/sendRewiescript.js
+++ hw3/sendRewiescript.js
@@ -1,66 +1,100 @@
-const sendRewie = document.querySelector('.sendRewie__form_Rewie');
-const productTitle = document.querySelector('.sendRewie__form_ProductTitle');
-const sendRewieButton = document.querySelector('.sendRewie__form_button');
-let products = [];
+(() => {
 
-// localStorage.clear();
-function checkProducts(product) {
-    if (!products.includes(product)) {
-        products.push(product);
-        localStorage.setItem("products", JSON.stringify(products));
+    function getToData(product) {
+        let temp = localStorage.getItem(product);
+        return JSON.parse(temp);
     }
-}
 
-function getToData(product) {
-    let temp = localStorage.getItem(product);
-    return JSON.parse(temp);
-}
-
-
-function checkRewie(product, rewie) {
-    let tempRewies = getToData(product);
-
-    if (!tempRewies) {
-        tempRewies = [];
-        tempRewies.push(rewie);
-        localStorage.setItem(product, JSON.stringify(tempRewies));
-    } else {
-        tempRewies.push(rewie);
-        localStorage.setItem(product, JSON.stringify(tempRewies));
+    function setToData(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
     }
-}
 
-sendRewie.addEventListener('click', function () {
-    if (this.value.length === '') {
-        this.setSelectionRange(0, 0);
+    function addListenersForSendRewieForm() {
+        const sendRewie = document.querySelector('.sendRewie__form_Rewie');
+        const productTitle = document.querySelector('.sendRewie__form_ProductTitle');
+        const sendRewieButton = document.querySelector('.sendRewie__form_button');
+
+        sendRewie.addEventListener('click', function () {
+            if (this.value.length === '') {
+                this.setSelectionRange(0, 0);
+            }
+        });
+
+        sendRewieButton.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            const product = productTitle.value;
+            const rewie = sendRewie.value;
+
+
+            if (checkValabylity(product, rewie)) {
+
+                checkProducts(product);
+
+
+                checkRewie(product, rewie);
+
+                sendRewie.value = '';
+                productTitle.value = '';
+
+
+            }
+        })
     }
-});
 
-function checkValabylity(product, rewie) {
-    if (product !== '' && rewie !== '') {
-        return true;
-    } else {
-        alert('Не все поля заполнены!!')
+    function checkValabylity(product, rewie) {
+        if (product !== '' && rewie !== '') {
+            return true;
+        } else {
+            alert('Не все поля заполнены!!')
+        }
     }
-}
 
-sendRewieButton.addEventListener('click', (e) => {
-    e.preventDefault();
+    function checkProducts(product) {
+        let products = getToData("products");
+        if (!products) {
+            products = [];
+            products.push(product)
+            setToData("products", products)
+        } else if (!products.includes(product)) {
+            products.push(product);
+            setToData("products", products)
 
-    const product = productTitle.value;
-    const rewie = sendRewie.value;
+        }
+    }
 
 
-    if (checkValabylity(product, rewie)) {
-
-        checkProducts(product);
 
 
-        checkRewie(product, rewie);
+    function checkRewie(product, rewie) {
+        let tempRewies = getToData(product);
+        let rewieId = Math.round(Math.random() * 1000);
+        rewie = {
+            rewie: rewie,
+            id: rewieId
+        };
 
-        sendRewie.value = '';
-        productTitle.value = '';
+        console.log(rewie);
+
+        if (!tempRewies) {
+            tempRewies = [];
+            tempRewies.push(rewie);
+            setToData(product, tempRewies)
+
+        } else {
+            tempRewies.push(rewie);
+            setToData(product, tempRewies)
+        }
+    }
+
+
+    function createSendRewiePage() {
+
+        // localStorage.cl
+        addListenersForSendRewieForm();
 
 
     }
-})
+
+    window.createSendRewiePage = createSendRewiePage;
+})();
--- hw3/watchRewieScript.js
+++ hw3/watchRewieScript.js
@@ -1,99 +1,183 @@
-const watchRewieMainContainer = document.querySelector('.watchRewie');
-let products = [];
+(() => {
+    function getToData(product) {
+        let temp = localStorage.getItem(product);
+        return JSON.parse(temp);
+    }
 
-function getToData(product) {
-    let temp = localStorage.getItem(product);
-    return JSON.parse(temp);
-}
+    function setToData(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
 
-if (localStorage.length !== 0) {
-    products = getToData("products");
-}
+    function createListOfProducts(products) {
+        const watchRewieMainContainer = document.querySelector('.watchRewie');
 
-// console.log(products);
-let listContainer = document.createElement('div');
-listContainer.classList.add('list__container');
+        if (products.length === 0) {
+            let listContainer = document.createElement('div');
+            listContainer.classList.add('list__container');
 
-let listOfProducts = document.createElement('ul');
-listOfProducts.classList.add('products__list');
-listOfProducts.textContent = 'Список продуктов на которые оставили отзывы:'
+            let noProductsWereRewied = document.createElement('p');
+            noProductsWereRewied.classList.add('no__products');
+            noProductsWereRewied.innerHTML = 'Пока что нет продуктов на которые оставили отзывы.<br> Вы можете быть первым! \=)'
+            noProductsWereRewied.style.textAlign = 'center';
 
-for (let i = 0; i < products.length; i++) {
-    let liProduct = document.createElement('li');
-    liProduct.classList.add('products__list_product');
-    liProduct.innerHTML = products[i]
+            listContainer.appendChild(noProductsWereRewied);
+            watchRewieMainContainer.appendChild(listContainer);
 
-    liProduct.addEventListener('mousedown', (e) => {
-        e.preventDefault();
+        } else {
 
-        listContainer.remove();
+            let noProducts = document.querySelector('.no__products')
 
-        showRewies(e.target.textContent);
-    })
-
-    listOfProducts.appendChild(liProduct);
-
-}
-
-function showRewies(product) {
-    const rewiesArray = getToData(product);
-    console.log(rewiesArray);
-
-    let productRewiesContainer = document.createElement('div');
-    productRewiesContainer.classList.add('product__rewies_container');
-
-    let productTitle = document.createElement('h2')
-    productTitle.textContent = product;
-
-    let rewiesContainer = document.createElement('div');
-    rewiesContainer.classList.add('rewies__container');
-    rewiesContainer.style.display = 'flex';
-    rewiesContainer.style.flexDirection = 'column';
-    rewiesContainer.style.gap = '15px';
-
-    for (let i = 0; i < rewiesArray.length; i++) {
-        let rewieWrapper = document.createElement('div');
-        rewieWrapper.classList.add('rewie__container');
-        rewieWrapper.style.display = 'flex';
-        rewieWrapper.style.flexDirection = 'column';
-        rewieWrapper.style.gap = '5px'
-
-        let rewie = document.createElement('p');
-        rewie.innerHTML = rewiesArray[i];
-        rewie.classList.add('rewie');
-
-        let deletRewieButton = document.createElement('button');
-        deletRewieButton.classList.add('delet__rewie_button');
-        deletRewieButton.textContent = 'Удалить комментарий'
-        deletRewieButton.style.padding = '10px 5px';
-        deletRewieButton.style.border = '1px solid red';
-        deletRewieButton.style.backgroundColor = 'red'
-
-        deletRewieButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            console.log(e.target);
-            console.log(e.currentTarget);
-
-            if (confirm('Вы точно хотите удалить комментарий?')) {
-
-                // for (let i = 0; i < rewiesArray.length; i++) {
-                //     if (rewiesArray[i].id === itemId) {
-                //         changingTasksList.splice(i, 1);
-                //         break;
-                //     }
-                // }
-
-                rewieWrapper.remove();
+            if (noProducts) {
+                noProducts.remove();
             }
+
+            let listContainer = document.createElement('div');
+            listContainer.classList.add('list__container');
+
+            let listOfProducts = document.createElement('ul');
+            listOfProducts.classList.add('products__list');
+            listOfProducts.textContent = 'Список продуктов на которые оставили отзывы:'
+
+            for (let i = 0; i < products.length; i++) {
+
+                if (checkRewies(products[i])) {
+                    let liProduct = document.createElement('li');
+                    liProduct.classList.add('products__list_product');
+                    liProduct.innerHTML = products[i]
+
+                    liProduct.addEventListener('mousedown', (e) => {
+                        e.preventDefault();
+
+                        listContainer.remove();
+
+                        showRewies(products, e.target.textContent);
+                    })
+
+                    listOfProducts.appendChild(liProduct);
+                }
+
+
+            }
+
+            listContainer.appendChild(listOfProducts);
+            watchRewieMainContainer.appendChild(listContainer);
+        }
+    }
+
+    function showRewies(products, product) {
+        const watchRewieMainContainer = document.querySelector('.watchRewie');
+
+        const rewiesArray = getToData(product);
+
+
+        let productRewiesContainer = document.createElement('div');
+        productRewiesContainer.classList.add('product__rewies_container');
+
+        let productTitle = document.createElement('h2')
+        productTitle.textContent = product;
+
+        let rewiesContainer = document.createElement('div');
+        rewiesContainer.classList.add('rewies__container');
+        rewiesContainer.style.display = 'flex';
+        rewiesContainer.style.flexDirection = 'column';
+        rewiesContainer.style.gap = '15px';
+
+        for (let i = 0; i < rewiesArray.length; i++) {
+            let rewieWrapper = document.createElement('div');
+            rewieWrapper.classList.add('rewie__container');
+            rewieWrapper.style.display = 'flex';
+            rewieWrapper.style.flexDirection = 'column';
+            rewieWrapper.style.gap = '5px'
+
+            let rewie = document.createElement('p');
+            rewie.innerHTML = rewiesArray[i].rewie;
+            rewie.classList.add('rewie');
+            rewie.setAttribute('id', rewiesArray[i].id);
+
+
+            let deletRewieButton = document.createElement('button');
+            deletRewieButton.classList.add('delet__rewie_button');
+            deletRewieButton.textContent = 'Удалить комментарий'
+            deletRewieButton.style.padding = '10px 5px';
+            deletRewieButton.style.border = '1px solid red';
+            deletRewieButton.style.backgroundColor = 'red'
+
+            deletRewieButton.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                if (confirm('Вы точно хотите удалить комментарий?')) {
+
+                    for (let i = 0; i < rewiesArray.length; i++) {
+                        if (rewiesArray[i].id === parseInt(rewie.id)) {
+                            rewiesArray.splice(i, 1);
+                            break;
+                        }
+                    }
+                    setToData(product, rewiesArray);
+                    rewieWrapper.remove();
+                }
+
+            })
+
+            rewieWrapper.append(rewie, deletRewieButton);
+            rewiesContainer.appendChild(rewieWrapper);
+        }
+        productRewiesContainer.append(productTitle, rewiesContainer);
+        watchRewieMainContainer.appendChild(productRewiesContainer);
+        getBackToProductsList(products);
+
+    }
+
+    function getBackToProductsList(products) {
+        const watchRewieMainContainer = document.querySelector('.watchRewie');
+        const rewiesContainer = document.querySelector('.product__rewies_container')
+
+        const backToProductsContainer = document.createElement('div');
+        backToProductsContainer.classList.add('backToProductList__container');
+        backToProductsContainer.style.display = 'flex';
+        backToProductsContainer.style.justifyContent = 'center'
+
+        const backButton = document.createElement('button');
+        backButton.classList.add('back__button');
+        backButton.textContent = 'Вернуться к списку продуктов';
+        backButton.style.backgroundColor = 'lightgreen'
+        backButton.style.border = '2px solid green';
+        backButton.style.padding = '10px 5px';
+
+        backButton.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            rewiesContainer.remove();
+            backToProductsContainer.remove();
+            createListOfProducts(products);
 
         })
 
-        rewieWrapper.append(rewie, deletRewieButton);
-        rewiesContainer.appendChild(rewieWrapper);
-    }
-    productRewiesContainer.append(productTitle, rewiesContainer);
-    watchRewieMainContainer.appendChild(productRewiesContainer)
-}
+        backToProductsContainer.appendChild(backButton);
+        watchRewieMainContainer.appendChild(backToProductsContainer);
 
-listContainer.appendChild(listOfProducts);
-watchRewieMainContainer.appendChild(listContainer);
+    }
+
+    function checkRewies(product) {
+        let rewies = getToData(product);
+
+        if (rewies.length > 0) {
+            return true;
+        }
+        return false;
+
+    }
+
+    function createWatchRewiePage() {
+
+        let products = [];
+
+        if (localStorage.length !== 0) {
+            products = getToData("products");
+        }
+
+        createListOfProducts(products);
+    }
+
+    window.createWatchRewiePage = createWatchRewiePage;
+})();
